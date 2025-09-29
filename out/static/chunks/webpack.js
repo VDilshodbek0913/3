b2b694b7378c6aa98437d89fe8cec4f8ef636d74
VDilshodbek0913 <yusufdavronov959@gmail.com>
2025-09-29 14:44:44 +0300

    Updated build-manifest.json

--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0686a6644a32efb5"; }
+/******/ 		__webpack_require__.h = function() { return "d662fd40b921cf79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
