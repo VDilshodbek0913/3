40e5af179ea61a62d2eb0498295a5091677192aa
VDilshodbek0913 <yusufdavronov959@gmail.com>
2025-09-29 14:49:12 +0300

    Fix Static Export and Improve Blog System

--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "56126ae2cc301739"; }
+/******/ 		__webpack_require__.h = function() { return "0f8c6865066dee0e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
