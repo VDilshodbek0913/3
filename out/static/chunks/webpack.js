0249d2abcc493948fb80ef5a365bf94055ded13a
VDilshodbek0913 <yusufdavronov959@gmail.com>
2025-09-30 10:50:26 +0300

    Fix API Errors and Add Comprehensive Logging

--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd986efae4907544"; }
+/******/ 		__webpack_require__.h = function() { return "b92c10e9cbc109f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
